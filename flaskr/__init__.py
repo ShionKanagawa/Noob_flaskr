15d5cee0f91bca0b8b82588b21c63752079715cf
Shirokane Shizuku <nickyundt432@gmail.com>
2023-05-06 09:38:19 +0800

    Add authrization fuctions
    * add login and register function and html templates from tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,8 +6,12 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    # import database functions.
     from . import db
     db.init_app(app)
+    # import authorization functions.
+    from . import auth
+    app.register_blueprint(auth.bp)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
