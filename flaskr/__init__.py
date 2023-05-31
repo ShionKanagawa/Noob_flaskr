ece9742725c4eb588b64f53a57fe8e2e6ec02e8a
Shirokane Shizuku <nickyundt432@gmail.com>
2023-05-31 11:35:41 +0800

    Fix-up!

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,6 +12,11 @@ def create_app(test_config=None):
     # import authorization functions.
     from . import auth
     app.register_blueprint(auth.bp)
+    # import blog functions..... Finally
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
